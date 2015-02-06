5c3458af9a24264eaeb77fcc605e3351f87a08c7
Stephane <stephane@MacBook-Pro-de-Stephane.local>
2015-02-06 16:44:38 +0100

    changes

--- app/2_js/main.js
+++ app/2_js/main.js
@@ -1,1 +1,11 @@
-console.log(calcul(3,8));
+$(document).ready(function(){
+    $('.content').each(function(){
+        var $bgobj = $(this); // assigning the object
+        $window = $(window);
+        $window.scroll(function() {
+            var yPos = -($window.scrollTop() /5);
+            var coords = '50% '+ yPos + 'px';
+            $bgobj.css({ backgroundPosition: coords });
+        });
+    });
+});
